a64d859c5f6f46c48a36f013b5c96d195b24ad28
Jayla House <danyeldev01@gmail.com>
2022-10-16 03:41:46 -0400

    Correct Toppings
    Depending on the drop the topping on the sandwich will change accordingly.

--- 625SandwitchMaker/Assets/Scripts/sandwitchScript.cs
+++ 625SandwitchMaker/Assets/Scripts/sandwitchScript.cs
@@ -7,10 +7,35 @@ public class sandwitchScript : MonoBehaviour
     public GameObject[] stackedSandwitch;
     public Sprite[] goodIngre;
 
+    public PrefabScript ps;
+
     void OnTriggerEnter2D(Collider2D col)
     {
         if(col.CompareTag("GoodIngre")){
-            stackedSandwitch[0].GetComponent<SpriteRenderer>().sprite = goodIngre[0];
+            if(col.GetComponent<SpriteRenderer>().sprite.name == "ingrediants_0"){  //bacon
+                stackedSandwitch[0].GetComponent<SpriteRenderer>().sprite = goodIngre[0];
+            }
+            if(col.GetComponent<SpriteRenderer>().sprite.name == "ingrediants_1"){  //onions
+                stackedSandwitch[0].GetComponent<SpriteRenderer>().sprite = goodIngre[1];
+            }
+            if(col.GetComponent<SpriteRenderer>().sprite.name == "ingrediants_2"){   //cheese
+                stackedSandwitch[0].GetComponent<SpriteRenderer>().sprite = goodIngre[2];
+            }
+            if(col.GetComponent<SpriteRenderer>().sprite.name == "ingrediants_3"){   //tomatoes
+                stackedSandwitch[0].GetComponent<SpriteRenderer>().sprite = goodIngre[3];
+            }
+            if(col.GetComponent<SpriteRenderer>().sprite.name == "ingrediants_4"){  //Ham
+                stackedSandwitch[0].GetComponent<SpriteRenderer>().sprite = goodIngre[4];
+            }
+            if(col.GetComponent<SpriteRenderer>().sprite.name == "ingrediants_5" || col.GetComponent<SpriteRenderer>().sprite.name == "ingrediants_6" ){  //mayo must
+                stackedSandwitch[0].GetComponent<SpriteRenderer>().sprite = goodIngre[5];
+            }
+            if(col.GetComponent<SpriteRenderer>().sprite.name == "ingrediants_7"){  //pickles
+                stackedSandwitch[0].GetComponent<SpriteRenderer>().sprite = goodIngre[6];
+            }
+
+            
+            Debug.Log(col.GetComponent<SpriteRenderer>().sprite.name);
         }
     }
 }
