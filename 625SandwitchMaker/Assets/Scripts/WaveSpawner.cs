e0be62aa640710905e930f80382fcc1e79a8ae90
Jayla House <danyeldev01@gmail.com>
2022-10-16 04:22:29 -0400

    Made random.range a float
    mad it 2f- (-2f)

--- 625SandwitchMaker/Assets/Scripts/WaveSpawner.cs
+++ 625SandwitchMaker/Assets/Scripts/WaveSpawner.cs
@@ -13,7 +13,7 @@ public class WaveSpawner : MonoBehaviour
     IEnumerator SpawnWave(int waveSize){
         yield return new WaitForSeconds(5);
         for(int i = 0; i <= waveSize; i++){
-            Instantiate(GoodObjectPrefab, new Vector3(Random.Range(2, -2), 3, 0), Quaternion.identity);
+            Instantiate(GoodObjectPrefab, new Vector3(Random.Range(2f, -2f), 3, 0), Quaternion.identity);
             yield return new WaitForSeconds(5);
         }
     }
