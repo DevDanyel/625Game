ed5b886711876b9bace4f319465abff6e9577879
Jayla House <danyeldev01@gmail.com>
2022-10-16 23:45:37 -0400

    HowToPlayScene
    Added and coded in the how to play button and screen

--- 625SandwitchMaker/Assets/Scripts/GameManager.cs
+++ 625SandwitchMaker/Assets/Scripts/GameManager.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject startScreen;
+    public GameObject HowToPlayScreen;
     public GameObject spawner;
 
     void Start(){
         startScreen.SetActive(true);
+        HowToPlayScreen.SetActive(false);
         spawner.SetActive(false);
     }
 
     public void startGame(){
         startScreen.SetActive(false);
+        HowToPlayScreen.SetActive(false);
         spawner.SetActive(true);
+    }
+
+    public void HowToPlay(){
+        HowToPlayScreen.SetActive(true);
+        startScreen.SetActive(false);
     }
 
 
