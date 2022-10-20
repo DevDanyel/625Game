3b895af988854a06e8d41a253151bc0c42471a89
Jayla House <danyeldev01@gmail.com>
2022-10-20 00:26:14 -0400

    fixed Ui and worked on spawner
    spanwer needs a little work but i think i figured out the whole alignment of buttons and ui

--- 625SandwitchMaker/Assets/Scripts/WaveSpawner.cs
+++ 625SandwitchMaker/Assets/Scripts/WaveSpawner.cs
@@ -5,18 +5,27 @@ using UnityEngine;
 public class WaveSpawner : MonoBehaviour
 {
     public GameObject GoodObjectPrefab;
+    public GameObject BadObjectPrefab;
     public GameObject topbread;
 
 
     void OnEnable(){
-        StartCoroutine(SpawnWave(6));
+        StartCoroutine(SpawnWave(100));
     }
 
     IEnumerator SpawnWave(int waveSize){
-        yield return new WaitForSeconds(5);
+        int goodOrBad = Random.Range(0, 1);
+        yield return new WaitForSeconds(3);     //create a count down for when starting the game.
         for(int i = 0; i <= waveSize; i++){
-            Instantiate(GoodObjectPrefab, new Vector3(Random.Range(2f, -2f), 3, 0), Quaternion.identity);
-            yield return new WaitForSeconds(5);
+            switch (goodOrBad) {
+                case 0:
+                Instantiate(GoodObjectPrefab, new Vector3(Random.Range(2f, -2f), 3, 0), Quaternion.identity);
+                break; 
+                case 1: 
+                Instantiate(BadObjectPrefab, new Vector3(Random.Range(2f, -2f), 3, 0), Quaternion.identity);
+                break;
+            }
+            yield return new WaitForSeconds(Random.Range(0, 5));
         }
         Instantiate(topbread, new Vector3(Random.Range(2f, -2f), 3, 0), Quaternion.identity);
     }
