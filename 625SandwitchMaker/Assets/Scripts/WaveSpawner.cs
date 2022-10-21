967d1140458cb7aec2e10e9bec4e80b21a4643eb
Jayla House <danyeldev01@gmail.com>
2022-10-21 19:59:25 -0400

    Better Range for object spawning
    made the range bigger making it more difficult.

--- 625SandwitchMaker/Assets/Scripts/WaveSpawner.cs
+++ 625SandwitchMaker/Assets/Scripts/WaveSpawner.cs
@@ -22,15 +22,15 @@ public class WaveSpawner : MonoBehaviour
         yield return new WaitForSeconds(3);     //create a count down for when starting the game.
         for(int i = 0; i <= waveSize; i++){
             if(goodOrBad >= 1.5f){
-                Instantiate(GoodObjectPrefab, new Vector3(Random.Range(2f, -2f), 3, 0), Quaternion.identity);
+                Instantiate(GoodObjectPrefab, new Vector3(Random.Range(3f, -7.6f), 3, 0), Quaternion.identity);
 
             }else if(goodOrBad < 1.5f){
-                Instantiate(BadObjectPrefab, new Vector3(Random.Range(2f, -2f), 3, 0), Quaternion.identity);
+                Instantiate(BadObjectPrefab, new Vector3(Random.Range(3f, -7.6f), 3, 0), Quaternion.identity);
 
             }
             //turn the 7 into a variable later so when levels get higher you have to wait longer to finish
-            if(i%7 == 0){
-                Instantiate(topbread, new Vector3(Random.Range(2f, -2f), 3, 0), Quaternion.identity);
+            if(i%7 == 0 && i >0){
+                Instantiate(topbread, new Vector3(Random.Range(3f, -7.6f), 3, 0), Quaternion.identity);
             }
             Debug.Log("i:"+ i+ "  |  i/7: " + i/7 +" |  GoodOrBad:"+ goodOrBad );
             yield return new WaitForSeconds(Random.Range(3f, ws));
